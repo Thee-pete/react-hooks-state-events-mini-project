43fb8ef8be241b96833aa8c2941a95e61eb27ca3
Peter Ndumia <ndegwandumia@gmail.com>
2022-07-21 17:43:14 +0300

    commit tree fc8e6ad90719efc2f95854dcb2707d2172b769cd

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,22 @@
 import React from "react";
 
-function NewTaskForm() {
+function NewTaskForm({categories , onTaskFormSubmit, onAddNewText, onAddNewCategory}) {
+
+  const validCats = categories.filter(category=>category !== "All");
+  const validCatsList = validCats.map(validCat=>{
+    return <option key={validCat}>{validCat}</option>
+  });
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={onTaskFormSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text"  onChange = {onAddNewText}/>
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" onChange={onAddNewCategory}>
+          {validCatsList}
         </select>
       </label>
       <input type="submit" value="Add task" />
