cf9188d15e7df0b4dcddbb4e1db98322428dc4fb
Peter Ndumia <ndegwandumia@gmail.com>
2022-07-21 17:47:10 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,64 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [category, setCategory] = useState("All");
+  const [newText, setNewText] = useState("");
+  const [newCategory, setNewCategory] =useState("");
+
+
+  function addNewText(e){
+    const updateText = e.target.value;
+    setNewText(updateText);
+  }
+  function addNewCategory(e){
+    const updateCategory = e.target.value;
+    setNewCategory(updateCategory);
+  }
+
+  function addNewTask(e){
+    e.preventDefault();
+      const newTask = {
+      text: newText,
+      category: newCategory,
+    }
+
+    const newTasks  = [...items, newTask];
+    setTasks(newTasks);
+  }
+
+
+  function removeTask(taskToDelete){
+    setTasks(tasks.filter((task) => {
+      task.text !== taskToDelete;
+
+    }))
+
+  }
+  function filterCategories(e){
+    e.target.className = "selected";;
+    let value = e.target.value;
+    setCategory(value);
+    if(value === "All"){
+      setTasks(TASKS);
+    }else{
+      const updatedTasks =tasks.filter(task => task.category === value);
+      setTasks(updatedTasks);
+    }
+  }
+
+
+
+
+
+
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories = {CATEGORIES} filterCategories = {filterCategories}  />
+      <NewTaskForm categories= {CATEGORIES} onTaskFormSubmit= {addNewTask} onAddNewText= {addNewText} onAddNewCategory = {addNewCategory}/>
+      <TaskList tasks={tasks} removeTask= {removeTask}/>
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React ,{useState} from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories ,filterCategories}) {
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      { categories.map((category) => {
+        return <button key = {category} onClick={filterCategories} value={category} >{category}</button>
+         })}
+     
     </div>
   );
 }
